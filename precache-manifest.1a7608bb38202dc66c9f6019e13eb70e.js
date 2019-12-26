b5345c34f7e1065a1aad9b286f1493c666fb5214
adedaniel <adetoladaniel693@gmail.com>
2019-12-26 22:32:44 +0100

    Updates

---
+++ precache-manifest.1a7608bb38202dc66c9f6019e13eb70e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336b7f03cbcb3c15e4fbbb801c88054f",
+    "url": "/about/index.html"
+  },
+  {
+    "revision": "aa5f28a6b32bf00e8593",
+    "url": "/about/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "f69a9c1746fc3b7c6fba",
+    "url": "/about/static/css/main.04b6d8e5.chunk.css"
+  },
+  {
+    "revision": "aa5f28a6b32bf00e8593",
+    "url": "/about/static/js/2.13356cd9.chunk.js"
+  },
+  {
+    "revision": "f69a9c1746fc3b7c6fba",
+    "url": "/about/static/js/main.62b6d42e.chunk.js"
+  },
+  {
+    "revision": "5e7b65462749e51da51f",
+    "url": "/about/static/js/runtime~main.c0e869d3.js"
+  }
+]);
